52d823fec91d03be28e43cc81795ba4b3bb50c37
Rémi Pelhate <remi.pelhate@icloud.com>
2020-03-15 18:27:27 +0100

    Add About section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Landing from 'src/components/home/Landing'
+import About from 'src/components/home/About'
 
 export default function HomePage() {
     return (
         <>
             <Landing />
+            <About />
         </>
     )
 }
