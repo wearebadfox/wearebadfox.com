511b4d581df764e43650956e33bd83ba314fa489
Rémi Pelhate <remi.pelhate@icloud.com>
2020-05-27 23:09:03 +0200

    Cleanup redundant code

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
-import LandingSection from 'src/pages/home/LandingSection'
-import AboutSection from 'src/pages/home/AboutSection'
-import ContactSection from 'src/pages/home/ContactSection'
 
 export default function HomePage() {
     return (
         <>
-            <LandingSection />
-            <AboutSection />
-            <ContactSection />
+            Bad fox
         </>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,49 +1,11 @@
-/**
- * @see https://tailwindcss.com/docs/theme/#app
- */
 module.exports = {
     theme: {
-        screens: {
-            sm: '640px',
-            md: '832px',
-            lg: '1024px',
-            xl: '1280px',
-            portrait: {
-                raw: '(orientation: portrait)',
-            },
-        },
         fontFamily: {
-            sans: ['Khula', 'sans-serif'],
-            title: ['Raleway', 'sans-serif'],
-        },
-        backgroundSize: {
-            auto: 'auto',
-            cover: 'cover',
-            contain: 'contain',
-            none: '0',
-            '3/2': '150%',
-        },
-        backgroundPosition: {
-            bottom: 'bottom',
-            center: 'center',
-            left: 'left',
-            right: 'right',
-            top: 'top',
-            'left-12': '12rem',
-            'left--14': '-14rem',
+            default: ['Jost', 'sans-serif'],
         },
         colors: {
-            black: '#121212',
-            white: '#fff',
-            gray: {
-                100: '#fafafa',
-                200: '#f0f2f5',
-                300: '#d7dde5',
-                400: '#c5cdd8',
-            },
-            cherry: '#eb3349',
-            orange: '#f25f44',
-            purple: '#8f26d9',
+            black: 'rgb(13, 13, 13)',
+            white: 'rgb(255, 255, 255)',
         },
         linearGradientDirections: {
             b: 'to bottom',
@@ -51,22 +13,6 @@ module.exports = {
             l: 'to left',
         },
         linearGradientColors: {
-            wiretrap: ['#f25f44', '#eb3349', '#8f26d9'],
-            cherry: {
-                orange: ['#f25f44', '#eb3349'],
-                purple: ['#eb3349', '#8f26d9'],
-            },
-            transparent: {
-                full: ['rgba(255, 255, 255, 0)', 'rgba(255, 255, 255, 0.8)'],
-                white: ['#fff 50%', 'rgba(255, 255, 255, 0.8)'],
-            },
-        },
-        boxShadow: {
-            backdrop: '0 10px 30px 7px rgba(244, 47, 74, 0.25)',
-            'backdrop-lg': '0 10px 30px 14px rgba(143, 38, 217, 0.3)',
-        },
-        textShadow: {
-            black: '0 1px 5px #121212',
         },
     },
     variants: {
