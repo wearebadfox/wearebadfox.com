eb3613f2ac05b8a18cef03a8fb78ac441270b612
Rémi Pelhate <remi.pelhate@icloud.com>
2020-05-27 22:52:59 +0200

    Add site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,9 @@
-/**
- * @see https://www.gatsbyjs.org/docs/gatsby-config/
- */
 module.exports = {
+    siteMetadata: {
+        title: 'Bad Fox',
+        description: 'Alternative rock band from Antwerp.',
+        author: 'Rémi Pelhate <remi.pelhate@icloud.com',
+    },
     plugins: [
         'gatsby-plugin-postcss',
         'gatsby-plugin-root-import',
