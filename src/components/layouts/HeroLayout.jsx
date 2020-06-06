8b12fbf628c976cd6b68b43854ed9abd235a9989
Rémi Pelhate <remi.pelhate@icloud.com>
2020-06-06 22:58:44 +0200

    Dynamically set min height of the hero image based on main content

--- src/components/layouts/HeroLayout.jsx
+++ src/components/layouts/HeroLayout.jsx
@@ -4,14 +4,16 @@ import HeroHeader from 'src/components/media/HeroHeader'
 import Footer from 'src/components/sections/Footer'
 
 export default function HeroLayout({ children, file, renderHeroHeader }) {
+    const isFullPage = typeof children === 'undefined';
+
     return (
         <>
             <Topbar />
             <div className="flex flex-col justify-between min-h-screen">
-                <HeroHeader file={file}>
+                <HeroHeader isFullPage={isFullPage} file={file}>
                     {renderHeroHeader()}
                 </HeroHeader>
-                <main>{children}</main>
+                <main className="flex-1">{children}</main>
                 <Footer />
             </div>
             <aside id="overlays" />
--- src/components/media/HeroHeader.jsx
+++ src/components/media/HeroHeader.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import classNames from 'classnames'
 import Img from 'gatsby-image'
 
-export default function HeroHeader({ file, children }) {
+const DEFAULT = 'min-h-l';
+const FULL_PAGE = 'min-h-2xl';
+
+export default function HeroHeader({ isFullPage, file, children }) {
+    const conditionalClasses = {
+        [DEFAULT]: !isFullPage,
+        [FULL_PAGE]: isFullPage,
+    };
+
     return (
-        <header className="relative min-h-2xl sm:min-h-md sm:max-h-lg">
+        <header className={classNames(conditionalClasses, 'relative sm:min-h-md sm:max-h-lg')}>
             <Img fluid={file.childImageSharp.fluid} className="max-w-6xl mx-auto min-h-md" />
             <div className="w-full h-full absolute top-0 bg-gradient-b-transparent-black flex items-end">
                 {children}
